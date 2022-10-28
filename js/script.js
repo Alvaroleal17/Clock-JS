f41c8d5c5f3d7429b8192c85b1e920210580e617
Alvarom Peña <alvaroandres0817@gmail.com>
2022-10-28 02:38:07 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let seconds = document.getElementById("seconds");
+let period = document.getElementById("period");
+
+const clock = setInterval(function time() {
+  let date_now = new Date();
+  let hr = date_now.getHours();
+  let min = date_now.getMinutes();
+  let sec = date_now.getSeconds();
+  let per = "a.m";
+
+  if (hr < 10) {
+    hr = "0" + hr;
+  } else if (hr >= 12) {
+    per = "p.m";
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  hour.textContent = hr;
+  minute.textContent = min;
+  seconds.textContent = sec;
+  period.textContent = per;
+}, 1000);
